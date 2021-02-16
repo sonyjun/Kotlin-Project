27f4df9cc79676e8d4889f1fb7bc76e8e40f7de3
home sonny <thswnsxor111@naver.com>
2021-02-17 01:04:12 +0900

    2021-02-17 at home

--- Test/src/kotlinTest.kt
+++ Test/src/kotlinTest.kt
@@ -5,6 +5,9 @@ fun main(args: Array<String>) {
     println(maxOf(20,50))
     println(stringSum("hi","juntaek"))
     println(stringSum1("hi","juny"))
+    println(whileLoopSum(5))
+    println(describe(50))
+    ramdaTest()
 }
 fun sum1(a : Int, b: Int):Int{
     return a+b
@@ -38,9 +41,62 @@ fun testFunc(a : String, b: String):Int{
     }
 }
 //
-fun getStringLength(obj : Any) : Int?{
-    if(obj is String){
+fun getStringLength(obj : Any) : Int?{ // Any는 Object와 같이 최상위 클래스 느낌임.
+    if(obj is String){ // obj가 String 객체라면 자동 타입 변환을 함.
+                        // 타입 캐스팅 할 때 발생할 수 있엇던 오류를 줄일 수 있음.
         return obj.length
     }
     return null
 }
+
+fun whileLoopSum(a:Int):Int{ // while loop : java와 동일.
+    var i :Int = 1
+    var sum : Int = 0
+    while(i <= a){
+        sum += i++
+    }
+    return sum
+}
+
+//when expression : java의 switch 같은 것인데,
+//다양한 타입을 받을 수 잇음.
+fun describe(obj : Any):String =
+    when(obj){
+        1 -> "one"
+        "hello" -> "Greeting"
+        is Long -> "Long"
+        !is String -> "Not a String"
+        else -> "Unknown"
+    }
+
+//range를 이용한 for loop 가능, if 문도 가능....
+fun range(a : Int, b : Int) : Unit{
+    var x : Int
+    for(x in a..b){
+        println(x)
+    }
+    if(a in 1..5){//a가 1~5사이에 있는지.
+        println("a is in from 1 to 5")
+    }
+}
+
+fun collections(){// collection활용 가능.
+// arraylist, linkedList, Hashset,treeset, hashmap,treemap 등등
+    val items1 = listOf("apple","banana","kiwi")
+    for(item in items1){
+        println(item)
+    }
+    val items2 = setOf("apple","banana","kiwi")
+    when{
+        "apple" in items2 -> "apple is exist"
+        "orange" in items2 -> "apple is exist"
+    }
+
+}
+
+fun ramdaTest(){
+    val fruits = listOf("banana","avocado","apple","kiwi")
+    fruits.filter{it.startsWith("a")}.sortedBy { it }.map { it.toUpperCase() }.forEach{println(it)}
+}
+
+// + 람다식을 이용해 filter, map 등의 연산이 가능하다.
